be29c62eb2644ea44f9497ce4748688c561457b3
Mikhail Gusarov <misha@ridge.co>
2023-06-27 21:23:30 +0200

    tmp

--- thttp/server.go
+++ thttp/server.go
@@ -8,6 +8,7 @@ import (
 	"go.uber.org/zap"
 	"net"
 	"net/http"
+	"runtime/debug"
 	"sync"
 	"sync/atomic"
 
@@ -59,10 +60,59 @@ func (g *Group) Spawn(name string, onExit parallel.OnExit, task parallel.Task) {
 	go g.runTask(tlog.WithLogger(g.ctx, logger), id, name, onExit, task)
 }
 
+// ErrPanic is the error type that occurs when a subtask panics
+type ErrPanic struct {
+	value any
+	stack []byte
+}
+
+func (err ErrPanic) Error() string {
+	return fmt.Sprintf("panic: %s", err.value)
+}
+
+// Unwrap returns the error passed to panic, or nil if panic was called with
+// something other than an error
+func (err ErrPanic) Unwrap() error {
+	if e, ok := err.value.(error); ok {
+		return e
+	}
+	return nil
+}
+
+// Value returns the value passed to panic
+func (err ErrPanic) Value() any {
+	return err.value
+}
+
+// Stack returns the panic stack trace
+func (err ErrPanic) Stack() []byte {
+	return err.stack
+}
+
+// RunTask executes the task in the current goroutine, recovering from panics.
+// A panic is logged, reported to monitoring and returned as ErrPanic.
+func RunTask(ctx context.Context, task parallel.Task) (err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			panicErr := ErrPanic{value: p, stack: debug.Stack()}
+			err = panicErr
+			tlog.Get(ctx).Error("Panic", zap.String("value", fmt.Sprint(p)), zap.ByteString("stack", panicErr.stack))
+			if panicCounter := ctx.Value(PanicCounterKey); panicCounter != nil {
+				panicCounter.(func())()
+			}
+		}
+	}()
+	return task(ctx)
+}
+
+type contextKey int
+
+const PanicCounterKey contextKey = iota
+
 // Second parameter is the task ID. It is ignored because the only reason to
 // pass it is to add it to the stack trace
 func (g *Group) runTask(ctx context.Context, _ int64, name string, onExit parallel.OnExit, task parallel.Task) {
-	err := parallel.RunTask(ctx, task)
+	err := RunTask(ctx, task)
 	tlog.Get(ctx).Debug("Task finished", zap.Error(err))
 
 	g.mu.Lock()
